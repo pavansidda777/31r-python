6fbe702ab587dea52ef689322e4dd7d6df36b569
pavan sidda <109956547+pavansidda777@users.noreply.github.com>
2025-02-04 20:22:44 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,70 @@
+#print single number
+print(2)
+
+#print multiple number
+print(1,2,3,45)
+
+
+#print with out variables
+print(3+7)#addition
+
+print(4-5)#subtraction
+
+print(3*4)#multiplication
+
+print(8/2)#division
+
+print(4**2)#exponentiation
+
+print(8//7)#floor division =it prints only integer value
+
+
+#by using variables for arthematic operations
+a=6
+b=5
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a**b)
+
+print(a//b)
+
+#string
+name="pavan sidda"
+
+print(name)
+
+#type method is used to find the which data type like string,int,float.
+print(type(name))
+
+#len() its prints the length of the string
+print(len(name))
+
+
+#index[] its prints index of string
+print(name[6])
+
+print(name[-4])#print index from reverse
+
+#slicing: Python, [:] is a slice notation that creates a shallow copy of the entire sequence.
+print(name[0:10])
+
+print(name[:])
+
+print(name[0:10])
+
+print(name[:8])
+
+print(name[0:10:1])#step
+
+print(name[-10:-3])
+
+
+
+
